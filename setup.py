0ed9fca192ced3f7cbd5600425e3815da4a8823a
niuwenhao <niuwenhao@oceandark.com>
2019-11-01 17:41:09 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@ import sys, os
 with open('README.md', 'rb') as f:
     readme = f.read().decode('utf-8')
 
-install_requires = []
-
-with open('requirements.txt', 'rb') as f:
-    for req in f.readlines():
-        install_requires.append(req.strip())
+install_requires = [
+        'torch',
+        'torchvision',
+        'numpy',
+        'opencv-python',
+        ]
 
 setup(
     name='cvtorch',
-    version='0.0.1',
+    version='0.0.4',
     description='vision tools based on opencv',
     long_description=readme,
     author='iHateTa11B0y',
     author_email='1187203155@qq.com',
-    install_requires=[], #install_requires,
+    install_requires=install_requires,
     packages=["cvtorch"],
+
 )
